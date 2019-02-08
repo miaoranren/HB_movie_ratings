2c8d986dbe0319619c3cc04fe3086cdfc101b138
Hackbright Student <no-reply@hackbrightacademy.com>
2019-02-07 17:51:02 -0800

    predictive part half completed

--- model.py
+++ model.py
@@ -2,6 +2,7 @@
 
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from correlation import pearson
 
 # This is the connection to the PostgreSQL database; we're getting this through
 # the Flask-SQLAlchemy helper library. On this, we can find the `session`
@@ -32,6 +33,63 @@ class User(db.Model):
         """Provide helpful representation when printed."""
 
         return f"<User user_id={self.user_id} email={self.email}>"
+
+
+
+    def similarity(self, other):
+        """Return Pearson rating for user compared to other user."""
+
+        u_ratings = {}
+        paired_ratings = []
+
+        for r in self.ratings:
+            u_ratings[r.movie_id] = r
+
+        for r in other.ratings:
+            u_r = u_ratings.get(r.movie_id)
+            if u_r:
+                paired_ratings.append( (u_r.score, r.score) )
+
+        if paired_ratings:
+            return pearson(paired_ratings)
+
+        else:
+            return 0.0
+
+
+    def predict_rating(self, movie):
+        """Predict user's rating of a movie."""
+
+        other_ratings = movie.ratings
+
+        similarities = [
+            (self.similarity(r.user), r)
+            for r in other_ratings
+        ]
+
+        similarities = sorted(similarities, reverse=True, key=lambda x: x[0])
+        sim, rating = similarities[0]
+
+        return rating.score * sim
+
+    # def predict_rating(self, movie):
+    #     """Predict user's rating of a movie."""
+
+    #     other_ratings = movie.ratings
+    #     other_users = [ r.user for r in other_ratings ]
+
+    #     similarities = [
+    #         (self.similarity(other_user), other_user)
+    #         for other_user in other_users
+    #     ]
+
+    #     similarities = sorted(similarities, reverse=True, key=lambda x: x[0])
+    #     sim, best_match_user = similarities[0]
+
+    #     for rating in other_ratings:
+    #         if rating.user_id == best_match_user.user_id:
+    #             return rating.score * sim
+
 
 
 # Put your Movie and Rating model classes here.
